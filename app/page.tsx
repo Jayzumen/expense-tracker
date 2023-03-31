d6a433159e57eb297f4d5085c4e20c04e1fcc4db
jayzumen <reinhardtjanniklas96@gmail.com>
2023-03-31 20:04:40 +0200

    fixed bug

--- app/page.tsx
+++ app/page.tsx
@@ -2,58 +2,6 @@ import { currentUser } from "@clerk/nextjs/app-beta";
 import type { User } from "@clerk/nextjs/dist/api";
 import NotLoggedIn from "./components/Utils/NotLoggedIn";
 import Dashboard from "./components/Dashboard";
-import getQueryClient from "./util/getQueryClient";
-import { Hydrate, dehydrate } from "@tanstack/react-query";
-import { db } from "@/lib/dbConn";
-import { expense, income } from "@/lib/schema";
-import { eq } from "drizzle-orm/expressions";
-import { Expense, Income } from "@/types/finances";
-
-const getExpenses = async () => {
-  const user: User | null = await currentUser();
-
-  const data = await db
-    .select()
-    .from(expense)
-    .where(eq(expense.user_id, user?.id as string))
-    .execute();
-  return data;
-};
-
-const getIncome = async () => {
-  const user: User | null = await currentUser();
-
-  const data = await db
-    .select()
-    .from(income)
-    .where(eq(income.user_id, user?.id as string))
-    .execute();
-  return data;
-};
-
-const getLimit = async () => {
-  const incomeData = await getIncome();
-
-  const incomeAmount: number = incomeData.reduce(
-    (acc: number, curr: Income) => {
-      return acc + curr.amount;
-    },
-    0
-  );
-
-  const expenseData = await getExpenses();
-
-  const expenseAmount: number = expenseData.reduce(
-    (acc: number, curr: Expense) => {
-      return acc + curr.amount;
-    },
-    0
-  );
-
-  const balance = incomeAmount - expenseAmount;
-
-  return balance;
-};
 
 export default async function HomePage() {
   const user: User | null = await currentUser();
@@ -61,15 +9,5 @@ export default async function HomePage() {
     return <NotLoggedIn />;
   }
 
-  // const queryClient = getQueryClient();
-  // await queryClient.prefetchQuery(["expenses"], getExpenses);
-  // await queryClient.prefetchQuery(["income"], getIncome);
-  // await queryClient.prefetchQuery(["limit"], getLimit);
-  // const dehydratedState = dehydrate(queryClient);
-
-  return (
-    // <Hydrate state={dehydratedState}>
-    <Dashboard />
-    // </Hydrate>
-  );
+  return <Dashboard />;
 }
